4b7756a31dca306aa769b3a180b78eef5bae5aa8
dsgkid123-afk <dsgkid123@gmail.com>
2026-05-19 22:28:50 -1000

    1 more issue fixed

--- sebbyFrontV2/TakeSpeakingRecs.py
+++ sebbyFrontV2/TakeSpeakingRecs.py
@@ -7,16 +7,14 @@ import numpy as np
 extraAudio = np.array([], dtype=np.int16)
 
 def start_recording_speach():
-    #start necessary threads for VAD
     threading.Thread(target=vad.start_listening_loop, daemon=True).start()
     threading.Thread(target=vad.cleanup_buffer, daemon=True).start()
 
     
 def get_clip():
+    global extraAudio  # moved to top of function
     while True:
-        if vad.are_they_speaking()==False or extraAudio.size > 3:
-            # add extra audio to begining of clip so we dont cut off the start of sentences
-            global extraAudio
+        if vad.are_they_speaking() == False or extraAudio.size > 3:
             clip = vad.clip()
             if clip is not None:
                 clip = np.concatenate([extraAudio, clip])
@@ -25,9 +23,7 @@ def get_clip():
         time.sleep(.15)
         
 def sped_clip():
-    # add new clip to extraAudio
-    global extraAudio
+    global extraAudio  # already correct
     clip = vad.clip()
     if clip is not None:
         extraAudio = np.concatenate([extraAudio, clip])
-    return
